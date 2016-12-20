c0d5e67dc17816af1c52a29d4539b5d059f62d94
MATSUNAGA Ichiro <vikke.bsd@gmail.com>
2016-12-21 00:56:55 +0900

    token認証通過させわすれてた

--- app/controllers/punches_controller.rb
+++ app/controllers/punches_controller.rb
@@ -1,5 +1,6 @@
 class PunchesController < ApplicationController
   before_action :set_punch, only: [:show, :edit, :update, :destroy]
+  skip_before_action :login_check, only: :punch
 
   # GET /punches
   # GET /punches.json
